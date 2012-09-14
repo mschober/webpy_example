011a6fcca8f70a35fdfbc7b2a54a5b81b823354f
Mike Schober <mschober@cj.com>
2012-09-13 22:22:08 -0700

    trying out my own template

--- code.py
+++ code.py
@@ -2,6 +2,7 @@ import web
 render = web.template.render('templates/')
 
 urls = (
+    '/users/(.*)', 'users',
     '/(.*)', 'index'
 )
 
@@ -9,6 +10,10 @@ urls = (
 class index: 
     def GET(self, name):
         return render.index(name)
+
+class users:
+    def GET(self, user):
+        return render.users(user)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())

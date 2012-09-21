73058b22baeeaad7d22a66a927a0d969a4bb4179
Mike Schober <mschober@cj.com>
2012-09-20 21:02:27 -0700

    connecting to mysql. able to query the database for data

--- code.py
+++ code.py
@@ -1,19 +1,28 @@
 import web
+import MySQLdb as db
+
 render = web.template.render('templates/')
 
 urls = (
-    '/users/(.*)', 'users',
-    '/(.*)', 'index'
+    #'/users/(.*)', 'users',
+    #'/(.*)', 'index'
+    '/', 'index'
 )
 
 
 class index: 
-    def GET(self, name):
-        return render.index(name)
+    #def GET(self, name):
+    #    return render.index(name)
+    def GET(self):
+        todos = db.select('todo')
+        return render.index(todos)
 
 class users:
     def GET(self, user):
         return render.users(user)
+
+#create table todo ( id int not null primary key auto_increment, title varchar(100), created timestamp default now(), done boolean not null default 0  );
+db = web.database(dbn='mysql', user='busbeep', pw='DannyBoy6812', db='busbeep_dev')
 
 if __name__ == "__main__":
     app = web.application(urls, globals())

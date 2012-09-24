f79e14a9a69378459905b74b075cc668ea959cb9
Mike Schober <mschober@cj.com>
2012-09-23 20:04:53 -0700

    renamed route to sonar

--- app/test/models/test_route_model.py
+++ app/test/models/test_route_model.py
@@ -1,15 +1,15 @@
 from nose.tools import *
-from src.models import route
+from src.models import sonar
 
 class TestCode:
 
-    def test_route_should_exist(self): assert route
+    def test_sonar_should_exist(self): assert sonar
 
     def test_it_should_connect_to_the_database(self):
-        connection = route.connect()
+        connection = sonar.connect()
         assert connection
 
     def test_can_select_from_dual(self):
-        connection = route.connect()
+        connection = sonar.connect()
         rows = connection.query('select 1 from dual')
         assert len(rows) >= 1
